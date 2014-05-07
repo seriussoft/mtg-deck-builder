21a184269e3d4885632bc1ab516f3adafc7093cd
Nate VanBuskirk <nathan.vanbuskirk@seriussoft.com>
2014-05-07 00:00:43 -0500

    Added deck model aggregate properties
    You can now discern the following from the DeckModel:
    - total converted mana cost
    - average converted mana cost
    - total cost occurrence of each mana cost type
    - 1 for each monocolor cost available
    - 1 for each hybrid color cost available
    - 1 for colorless
    - total cost by groups
    - exa one group might be 1BB and another might be {2/U}{2/B}
    - these changes are backed by integration tests currently

--- MtgDeckBuilder-Shared/ModelTests/ManaParserIntegrationTests.cs
+++ MtgDeckBuilder-Shared/ModelTests/ManaParserIntegrationTests.cs
@@ -33,5 +33,62 @@ namespace ModelTests
       Assert.IsNotNull(card.ManaCost);
       Assert.AreEqualCollections(card.ManaCost.Costs, expectedComplexManaCost.Costs);
     }
+
+		[TestMethod]
+		public void ManaParserTests_Success_4LeoninSquire_ReturnsCorrectTotalConvertedCostAndAverageConvertedCost()
+		{
+			var db = new CardsDB();
+			var testCardName = "Leonin Squire";
+			var testCardNameUndercase = testCardName.ToLower();
+
+			var result = db.GetCardByName(testCardNameUndercase);
+			var card = result.Result;
+			var deck = new DeckModel(new[] { card, card, card, card });
+
+			Assert.AreEqual(8, deck.TotalConvertedManaCost);
+			Assert.AreEqual(2, deck.AverageConvertedManaCost);
+		}
+
+		[TestMethod]
+		public void ManaParserTests_Success_4LeoninSquire_ReturnsCorrectTotalActualCost()
+		{
+			var db = new CardsDB();
+			var testCardName = "Leonin Squire";
+			var testCardNameUndercase = testCardName.ToLower();
+
+			var result = db.GetCardByName(testCardNameUndercase);
+			var card = result.Result;
+			var deck = new DeckModel(new[] { card, card, card, card });
+
+			var totalCosts = deck.TotalCosts;
+
+			//Makes the assumption that we just want the total number of each monocolor used, each hybrid color used, and each colorless used
+			Assert.AreEqual(2, totalCosts.Count);
+			Assert.AreEqual(4, totalCosts[ManaColors.Colorless]);
+			Assert.AreEqual(4, totalCosts[ManaColors.White]);
+		}
+
+		[TestMethod]
+		public void ManaParserTests_Success_4LeoninSquire_ReturnsCorrectTotalGroupedCost()
+		{
+			var db = new CardsDB();
+			var testCardName = "Leonin Squire";
+			var testCardNameUndercase = testCardName.ToLower();
+			var expectedCost = "1W";
+			var convertedExpectedCost = new ManaCostModel(expectedCost);
+
+			var result = db.GetCardByName(testCardNameUndercase);
+			var card = result.Result;
+			var deck = new DeckModel(new[] { card, card, card, card });
+
+			var totalCosts = deck.TotalCostsByGroup;
+
+			//Makes the assumption that we just want the total number of each monocolor used, each hybrid color used, and each colorless used
+			Assert.AreEqual(1, totalCosts.Count);
+			Assert.IsNotNull(totalCosts.Keys.FirstOrDefault());
+			Assert.AreEqual(2, totalCosts.Keys.FirstOrDefault().Costs.Count);
+			Assert.AreEqualCollections(convertedExpectedCost.Costs, totalCosts.Keys.FirstOrDefault().Costs);
+			Assert.AreEqual(4, totalCosts.Values.FirstOrDefault());
+		}
   }
 }
--- MtgDeckBuilder-Shared/Models/DeckModel.cs
+++ MtgDeckBuilder-Shared/Models/DeckModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,61 +9,109 @@ using Models.QueryHelpers;
 namespace Models
 {
   public class DeckModel
-  {
-    public List<CardModel> Cards { get; private set; }
+	{
+		#region Properties
 
-    public DeckModel(IEnumerable<CardModel> cards = null)
+		public List<CardModel> Cards { get; private set; }
+
+		public int TotalConvertedManaCost { get { return this.Cards.Sum(c => c.ConvertedManaCost); } }
+		public double AverageConvertedManaCost { get { return this.Cards.Average(c => c.ConvertedManaCost); } }
+		public IDictionary<ManaColors, int> TotalCosts
+		{
+			get
+			{
+				var costs = this.Cards.SelectMany(c => c.ManaCost.Costs.ToList()).Select(kvp => new { Cost = kvp.Key, Total = kvp.Value });
+				var costTotals =
+					costs
+					.GroupBy(c => c.Cost)
+					.Select
+					(
+						grp => new KeyValuePair<ManaColors, int>
+						(
+							grp.Key, grp.Sum(g => g.Total)
+						)
+					).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+				// want it to be obvious that this is a readonly collection because at some point, 
+				// this will need to be offloaded due to api calls being so inefficient
+				return new ReadOnlyDictionary<ManaColors,int>(costTotals);
+			}
+		}
+
+		public IDictionary<ManaCostModel, int> TotalCostsByGroup
+		{
+			get 
+			{ 
+				var costs = this.Cards
+					.Select(c => c.ManaCostSimple)
+					.GroupBy(mc => mc)
+					.Select(grp => new KeyValuePair<ManaCostModel, int>(new ManaCostModel(grp.Key), grp.Count()))
+					.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+				return new ReadOnlyDictionary<ManaCostModel,int>(costs);
+			}
+		}
+
+		#endregion	Properties
+
+		public DeckModel(IEnumerable<CardModel> cards = null)
     {
       this.Cards = new List<CardModel>();
 
       AddCards(cards);
     }
 
-    public CardModel AddCard(CardModel card)
-    {
-      this.Cards.Add(card);
-      return card;
-    }
+		#region Card Add/Remove Methods
 
-    public IEnumerable<CardModel> AddCards(IEnumerable<CardModel> cards)
-    {
-      if (cards != null && cards.Any())
-        this.Cards.AddRange(cards);
+		public CardModel AddCard(CardModel card)
+		{
+			this.Cards.Add(card);
+			return card;
+		}
 
-      return cards;
-    }
+		public IEnumerable<CardModel> AddCards(IEnumerable<CardModel> cards)
+		{
+			if (cards != null && cards.Any())
+				this.Cards.AddRange(cards);
 
-    public CardModel RemoveCard(CardModel card)
-    {
-      if (this.Cards.Contains(card))
-        this.Cards.Remove(card);
+			return cards;
+		}
 
-      return card;
-    }
+		public CardModel RemoveCard(CardModel card)
+		{
+			if (this.Cards.Contains(card))
+				this.Cards.Remove(card);
 
-    public CardModel RemoveCard(string cardName)
-    {
-      var cardToRemove = this.Cards.FirstOrDefault(card => card.Name == cardName);
-      if (cardToRemove != null)
-      {
-        this.Cards.Remove(cardToRemove);
-        return cardToRemove;
-      }
+			return card;
+		}
 
-      return null;
-    }
+		public CardModel RemoveCard(string cardName)
+		{
+			var cardToRemove = this.Cards.FirstOrDefault(card => card.Name == cardName);
+			if (cardToRemove != null)
+			{
+				this.Cards.Remove(cardToRemove);
+				return cardToRemove;
+			}
 
-    public IEnumerable<CardModel> RemoveCards(params string[] cardNames)
-    {
-      var cardsToRemove = this.Cards.FindAll(c => cardNames.Contains(c.Name));
-      if (cardsToRemove.Any())
-      {
-        var cardsRemoved = cardsToRemove.ToList();
-        this.Cards.RemoveAll(c => cardNames.Contains(c.Name));
-        return cardsRemoved;
-      }
+			return null;
+		}
 
-      return new List<CardModel>();
-    }
+		public IEnumerable<CardModel> RemoveCards(params string[] cardNames)
+		{
+			var cardsToRemove = this.Cards.FindAll(c => cardNames.Contains(c.Name));
+			if (cardsToRemove.Any())
+			{
+				var cardsRemoved = cardsToRemove.ToList();
+				this.Cards.RemoveAll(c => cardNames.Contains(c.Name));
+				return cardsRemoved;
+			}
+
+			return new List<CardModel>();
+		} 
+		
+		#endregion	Card Add/Remove Methods
+
+
   }
 }
